90a40879b63982cb14caf59d5d72610630b7621f
reynaldlamury <51454984+reynaldlamury@users.noreply.github.com>
2022-11-20 11:55:16 +0700

    initial commit with simple npm startup files

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log('hellow beautiful world');
+
+let statement = 'im good today';
+
+const saying = (input) => {
+  console.log(input);
+};
+
+saying(statement);
+
+const number1 = 180;
+const number2 = '180';
+
+console.log(number1 + 1);
+console.log(number2 + 1);
